1ac9d1a88501f658a5b2ddb2c9be90449386c032
Grant Eadie <granteadie@Grants-iMac-2.local>
2023-11-04 13:38:28 -0700

    remove snakehips data.

--- app/data/shows.ts
+++ app/data/shows.ts
@@ -3,13 +3,6 @@ import { ShowClass } from "../comps/show";
 export const showData: ShowClass[] = [
   {
     city: "Seattle",
-    venue: "The Crocodile",
-    date: "October 14, 2023",
-    lineup: "Snakehips",
-    url: "https://www.tixr.com/groups/nectarlounge/events/gold-panda-plus-manatee-commune-dj-set--74337",
-  },
-  {
-    city: "Seattle",
     venue: "Nectar Lounge",
     date: "November 14, 2023",
     lineup: "Gold Panda",
